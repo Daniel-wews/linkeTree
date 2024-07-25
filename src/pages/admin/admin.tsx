df2bc6d335315ed1d996ae0b801483b39364ea17
Daniel Junior <daniel1236100@gmail.com>
2024-07-25 12:47:36 -0300

    começando a criar o formulario.

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import Header from "../../components/header/header";
+import Input from "../../components/input/input";
 
 export default function Admin(){
     return(
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header/>
+
+            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl ">
+                <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
+                <Input placeholder="digite o nome do link..."/>
+            </form>
         </div>
     )
 }
